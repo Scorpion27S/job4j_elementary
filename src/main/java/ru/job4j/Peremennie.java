d74d53bf99c22ad6b1df90c58abbd44ca55ffcb5
Echo.java <Koklyuhin-gleb@mail.ru>
2023-08-01 21:55:46 +0700

    Переменные

--- src/main/java/ru/job4j/Peremennie.java
+++ src/main/java/ru/job4j/Peremennie.java
@@ -2,11 +2,25 @@ package ru.job4j;
 
 public class Peremennie {
     public static void main(String[] args) {
-        int age = 18;
+        int age = 27;
         System.out.println(age);
-        float length = 78.0332f;
-        System.out.println(length);
-        boolean baby = true;
-        System.out.println(baby);
+        char man = 'M';
+        System.out.println(man);
+        char women = 'J';
+        System.out.println(women);
+        float height = 15f;
+        System.out.println(height);
+        short kirpich = 1000;
+        System.out.println(kirpich);
+        int baktery = 10000000;
+        System.out.println(baktery);
+        long kosmos = 254333666;
+        System.out.println(kosmos);
+        boolean work = true;
+        System.out.println(work);
+        boolean noWork = false;
+        System.out.println(noWork);
+        byte size = 121;
+        System.out.println(size);
     }
 }
