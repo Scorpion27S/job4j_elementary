c9aff810d858974d5afabbeaeeb02d22e41d8b02
Echo.java <Koklyuhin-gleb@mail.ru>
2023-08-02 20:18:44 +0700

    Переменные

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,26 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        System.out.println(sixDivTwo);
-        int fiveMinusTwo = five - two;
-        System.out.println(fiveMinusTwo);
-        int fourTimeTwo = four * two;
-        System.out.println(fourTimeTwo);
+public static void main(String[] args) {
+        byte age = 27;
+        System.out.println(age);
+        char man = 'M';
+        System.out.println(man);
+        char women = 'J';
+        System.out.println(women);
+        float height = 15f;
+        System.out.println(height);
+        short kirpich = 1000;
+        System.out.println(kirpich);
+        double baktery = 1.2345E+21;
+        System.out.println(baktery);
+        double kosmos = 1.6678E+21;
+        System.out.println(kosmos);
+        boolean work = true;
+        System.out.println(work);
+        boolean noWork = false;
+        System.out.println(noWork);
+        long size = 121;
+        System.out.println(size);
     }
 }
