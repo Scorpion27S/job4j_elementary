51104915612994740f59f2498ba37e0686eb034f
Echo.java <Koklyuhin-gleb@mail.ru>
2023-08-09 20:16:37 +0700

    9.3. Математические операторы и составные операторы присваивания

---
+++ src/main/java/ru/job4j/condition/StringRunner.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " But I am a newbie. ";
+        int year = 2022;
+        idea = idea + year;
+        System.out.println(idea);
+        idea += year;
+        System.out.println("summ: " + idea);
+        year -= idea;
+        System.out.println("subtract: " + year);
+        idea *= year;
+        System.out.println("multiply: " + idea);
+        year /= idea;
+        System.out.println("divide: " + year);
+    }
+}
