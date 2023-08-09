d5d3021061c8a1d1fcef7daedaeacfbd8e3f78d2
Echo.java <Koklyuhin-gleb@mail.ru>
2023-08-09 20:43:01 +0700

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -15,7 +15,16 @@ public class Converter {
     public static void main(String[] args) {
         double euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        double dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        double in = 140;
+        double expected = 2;
+        double out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        double dollar = Converter.rubleToDollar(60);
+        double in1 = 60;
+        double expected1 = 1;
+        double out1 = Converter.rubleToDollar(in1);
+        boolean pass = expected1 == out1;
+        System.out.println("60 rubles are 2. Test result : " + pass);
     }
 }
