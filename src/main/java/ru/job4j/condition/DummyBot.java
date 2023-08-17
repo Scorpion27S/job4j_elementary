7b5f53790b989cb11d107289ada2e3e7d068aa0e
Echo.java <Koklyuhin-gleb@mail.ru>
2023-08-17 18:54:40 +0700

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -5,9 +5,9 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            rsl = "Hi, SmartAss";
-        } else if ("Bye".equals(question)) {
-            rsl = "See you later";
+            rsl = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            rsl = "See you later.";
         }
         return rsl;
     }
