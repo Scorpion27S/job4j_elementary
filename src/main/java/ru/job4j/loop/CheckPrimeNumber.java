5ae34d021c5682c2074f363caad759a0fc782f16
Echo.java <Koklyuhin-gleb@mail.ru>
2023-08-29 20:20:52 +0700

    5.5. Простое число (оператор прерывания break) [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int i = 1; i <= number; i++) {
+            if (number / 2 == 0) {
+                prime = false;
+                break;
+            }
+            if (number % 2 == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
