7717ac737a673aca870b2202d407b65dc2ceaa64
Echo.java <Koklyuhin-gleb@mail.ru>
2023-09-08 20:33:18 +0700

    6.2. Упорядочить массив. [#4441]

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        int i = array[2];
+        array[2] = array[1];
+        array[1] = i;
+
+        int j = array[3];
+        array[3] = array[4];
+        array[4] = j;
+
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
